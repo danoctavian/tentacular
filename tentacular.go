c7c59358da79998869e0b5020b42605ef5ba1b68
Dan Octavian <danoctavian91@gmail.com>
2016-01-25 15:24:56 -0800

    removed dead code

--- tentacular.go
+++ tentacular.go
@@ -14,19 +14,6 @@ const MASTER = "master"
 const SLAVE = "slave"
 
 func main() {
-  /*
-	proxy := goproxy.NewProxyHttpServer()
-	proxy.Verbose = true
-
-	proxy.OnRequest().DoFunc(
-		func(r *http.Request,ctx *goproxy.ProxyCtx)(*http.Request,*http.Response) {
-			r.Header.Set("X-GoProxy","yxorPoG-X")
-			return r,nil
-  	})
-
-	log.Fatal(http.ListenAndServe(":8080", proxy))
-	*/
-
   proxyType := flag.String("type", SLAVE, "type of proxy. can be either master or slave")
   port := flag.Uint("port", 8080, "port of the proxy")
 
