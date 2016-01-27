a3ca4267cc79debeadc3cbd6e97f1b28ce1f15cd
Dan Octavian <danoctavian91@gmail.com>
2016-01-27 02:39:56 -0800

    fixed parameter reading for tentacular

--- tentacular.go
+++ tentacular.go
@@ -16,11 +16,12 @@ const SLAVE = "slave"
 
 func main() {
   proxyType := flag.String("type", SLAVE, "type of proxy. can be either master or slave")
+
   port := flag.Uint("port", 8080, "port of the proxy")
 
-  slaveport := uint16(*flag.Uint("slaveport", 6666, "port for slaves to contact"))
+  slaveport := flag.Uint("slaveport", 6666, "port for slaves to contact")
 
-  urlString := *flag.String("masterurl", "http://127.0.0.1:6666", "url of the master proxy")
+  urlString := flag.String("masterurl", "http://127.0.0.1:6666", "url of the master proxy")
 
   flag.Parse()
 
@@ -29,16 +30,16 @@ func main() {
   switch (*proxyType) {
   case MASTER:
     log.Print("Launching a master proxy")
-    proxyServer = NewMasterProxyServer(MasterProxyConfig{slaveProxyPort: slaveport})
+    proxyServer = NewMasterProxyServer(MasterProxyConfig{slaveProxyPort: uint16(*slaveport)})
   case SLAVE:
-    url, err := url.Parse(urlString)
+    url, err := url.Parse(*urlString)
 
     if err != nil {
       log.Fatalln("Failed to parse master URL")
       os.Exit(1)
     }
 
-    log.Print("Launching a slave proxy")
+    log.Print("Launching a slave proxy with a master at " + *urlString)
     proxyServer = NewSlaveProxyServer(SlaveProxyConfig{port: *port, masterURL: *url})
   }
 
